3df1be6d1cfaef1a21149c82f2759866d6c20cba
thuanht567 <96828228+thuanht567@users.noreply.github.com>
2024-02-24 18:39:52 +0700

    Add files via upload

---
+++ install567.sh
@@ -0,0 +1,1 @@
+pkg install wget -y && wget https://github.com/thuanht567/v2/raw/main/v2.zip && unzip v2.zip && cd && chmod +x * && echo 'PATH="$PATH:$HOME"' >> $HOME/.bashrc && source $HOME/.bashrc && t
