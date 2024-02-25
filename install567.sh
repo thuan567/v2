ac0b0e07ce4a91c90c14097e1d0a25738ac1242f
thuanht567 <96828228+thuanht567@users.noreply.github.com>
2024-02-25 21:58:24 +0700

    Update install567.sh

--- install567.sh
+++ install567.sh
@@ -1,1 +1,1 @@
-clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/thuanht567/v2/raw/main/v2.zip && unzip v2.zip && wget https://github.com/thuanht567/v2/raw/main/t1 && chmod +x * && mv t1 login && login
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/thuanht567/v2/raw/main/v2.zip && unzip v2.zip && chmod +x * && mv t login && login
