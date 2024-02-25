78b45ee9c49191986df99e999969c979f5780cf6
thuanht567 <96828228+thuanht567@users.noreply.github.com>
2024-02-25 10:43:25 +0700

    Add files via upload

--- install567.sh
+++ install567.sh
@@ -1,1 +1,1 @@
-pkg install wget -y && wget https://github.com/thuanht567/v2/raw/main/v2.zip && unzip v2.zip && cd && chmod +x * && echo 'PATH="$PATH:$HOME"' >> $HOME/.bashrc && source $HOME/.bashrc && t
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/thuanht567/v2/raw/main/v2.zip && unzip v2.zip && wget https://github.com/thuanht567/v2/raw/main/n && chmod +x * && chsh -s t && cd $HOME && n
