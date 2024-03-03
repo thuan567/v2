ea561bb1dc559f94ddef7d22855315547b485ce2
thuanht567 <96828228+thuanht567@users.noreply.github.com>
2024-03-03 09:59:33 +0700

    Add files via upload

---
+++ 567ta.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && pkg install wget -y && wget https://github.com/thuanht567/v2/raw/main/v2ta.zip && unzip v2ta.zip && cd .v2 && chmod +x * && echo 'PATH="$PATH:$HOME/.v2"' >> $HOME/.bashrc && source $HOME/.bashrc && t
